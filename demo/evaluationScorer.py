0cc588d828b6c8246594771de4ba6a9fab98e810
Maoc <wyc900212@163.com>
2023-10-24 18:19:12 +0800

    新增更新

--- demo/evaluationScorer.py
+++ demo/evaluationScorer.py
@@ -1,46 +1,43 @@
-import pandas as pd
+import execjs
 
 
-class EvaluationScorer:
-    def __init__(self, name_column, value, grade_columns):
-        self.name_column = name_column
-        self.value = value
-        self.grade_columns = grade_columns
-        self.file_path = "E:\dev_coding\yi_code\makingtable.xlsx"  # 表格文件路径
+class FormulaEvaluator:
+    def __init__(self, if_formula):
+        self.if_formula = if_formula
 
-    def calculate_score(self):
+    def evaluate(self, context):
+        # 替换公式中的变量
+        for key, value in context.items():
+            self.if_formula = self.if_formula.replace(key, str(value))
+
+        if self.if_formula != '':
+            # 执行替换后的公式
+            result = self.if_formula
+            return result
+        else:
+            return '公式不能为空'
+
+    @staticmethod
+    def execute_javascript(result):
         try:
-            # 读取Excel表格
-            data = pd.read_excel(self.file_path)
+            # 创建一个JavaScript环境
+            ctx = execjs.compile(result)
 
-            # 判断是否存在指定的列
-            if self.name_column not in data.columns:
-                return "指定的列名不存在"
-
-            # 初始化评分
-            score = 0
-
-            # 根据名称列匹配数据
-            matched_data = data[data[self.name_column] == self.value]
-
-            # 如果匹配到多行数据，选择第一行
-            if len(matched_data) > 0:
-                matched_row = matched_data.iloc[0]
-
-                # 根据评分区间列获取相应的分数
-                for grade_col in self.grade_columns:
-                    score += matched_row.get(grade_col, 0)
-
-                return score
+            # 执行JavaScript代码
+            results = ctx.call("main")
+            if results:
+                print(f"{results} 分")
             else:
-                return "未找到匹配的数据"
+                print('手动打分')
+
         except Exception as e:
             return str(e)
 
 
-# 实例化对象
-scorer = EvaluationScorer("name", "SampleName", ["grade_score0", "grade_score1", "grade_score2"])
+# 示例用法
+if_formula = 'if ( 性别 == "男" && [71:YTB] < 0.85 ) return 0; else if ( 性别 == "女" && [71:YTB] < 0.8 ) return 0;else if ( ( 性别 == "男" && [71:YTB] >= 0.85 && [71:YTB] < 0.89 ) || ( 性别 == "女" && [71:YTB] >= 0.8 && [71:YTB] < 0.84 ) ) return 1;else if ( ( 性别 == "男" && [71:YTB] >= 0.89 && [71:YTB] < 0.92 ) || ( 性别 == "女" && [71:YTB] >= 0.84 && [71:YTB] < 0.87 ) ) return 2;else if ( ( 性别 == "男" && [71:YTB] >= 0.92 && [71:YTB] < 0.95 ) || ( 性别 == "女" && [71:YTB] >= 0.87 && [71:YTB] < 0.9 ) ) return 3;else if ( ( 性别 == "男" && [71:YTB] >= 0.95 && [71:YTB] < 1 ) || ( 性别 == "女" && [71:YTB] >= 0.9 && [71:YTB] < 0.95 ) ) return 4;else if ( ( 性别 == "男" && [71:YTB] >= 1 ) || ( 性别 == "女" && [71:YTB] >= 0.95 ) ) return 5;'
+context = {"性别": '\"女\"', "[71:YTB]": 0.91}
 
-# 调用方法进行评分
-result = scorer.calculate_score()
-print("评分结果:", result)
+evaluator = FormulaEvaluator(if_formula)
+result = evaluator.evaluate(context)
+evaluator.execute_javascript(result)
